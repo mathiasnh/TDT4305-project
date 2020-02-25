5128a37c57bf1c3605f7e2295ab42865726ea0b5
AndreasArnholm <andrcar@ntnu.no>
2020-02-25 11:07:45 +0100

    XD

--- loadRDD.py
+++ loadRDD.py
@@ -3,7 +3,14 @@ from pyspark import SparkContext, SparkConf
 conf = SparkConf().setAppName("YelpReviews").setMaster("local")
 sc = SparkContext(conf=conf)
 
-dataFile = sc.textFile("yelp_business.csv")
+folder_name = "./data/"
 
-print(dataFile)
+input_file_name_1 = "yelp_business.csv"
+input_file_name_2 = "yelp_top_reviewers_with_reviews.csv"
+input_file_name_3 = "yelp_top_users_friendship_graph.csv"
 
+output_file_name = "result_1.csv"
+
+textFile_1 = sc.textFile(folder_name + input_file_name_1)
+textFile_2 = sc.textFile(folder_name + input_file_name_2)
+textFile_3 = sc.textFile(folder_name + input_file_name_3)
