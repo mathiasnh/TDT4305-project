bdc8ad62c9ad766ae8786a8f35af3ddf12b8b708
Mathias Henriksen <mnh@vitalthings.com>
2020-03-05 12:20:32 +0100

    task 4 done

--- task3.py
+++ task3.py
@@ -22,11 +22,11 @@ yelp_businesses_rdd = yelp_businesses.map(lambda line: line.split('\t'))
 
 yelp_businesses_rdd.cache()
 """ a """
-cities = yelp_businesses_rdd.map(lambda fields: (fields[3], float(fields[8])))
-tot_number_of_stars_per_city = cities.reduceByKey(lambda x, y: x + y)
-reviews_per_city = cities.map(lambda k: (k[0], 1)).reduceByKey(lambda x, y: x + y)
+#cities = yelp_businesses_rdd.map(lambda fields: (fields[3], float(fields[8])))
+#tot_number_of_stars_per_city = cities.reduceByKey(lambda x, y: x + y)
+#reviews_per_city = cities.map(lambda k: (k[0], 1)).reduceByKey(lambda x, y: x + y)
 
-result_a = tot_number_of_stars_per_city.join(reviews_per_city).map(lambda t: (t[0], t[1][0] / t[1][1]))
+#result_a = tot_number_of_stars_per_city.join(reviews_per_city).map(lambda t: (t[0], t[1][0] / t[1][1]))
 
 #ratings_per_city = users.map(lambda k: (k, 1)).reduceByKey(lambda x, y: x + y)
 
@@ -34,7 +34,7 @@ result_a = tot_number_of_stars_per_city.join(reviews_per_city).map(lambda t: (t[
 #categories = yelp_businesses_rdd.flatMap(lambda fields: fields[10].replace(" ", "").split(',')).map(lambda k: (k, 1)).reduceByKey(lambda x, y: x + y)
 
 """ c """
-"""
+
 postal_codes = yelp_businesses_rdd.map(lambda fields: fields[5])
 num_of_businesses_in_pc = postal_codes.map(lambda k: (k, 1)).reduceByKey(lambda x, y: x + y)
 
@@ -47,9 +47,10 @@ lats_sum = lats.reduceByKey(lambda x, y: x + y)
 joined = num_of_businesses_in_pc.join(longs).join(lats)
 
 yelp_businesses_rdd.unpersist()
-"""
+
 
 #count_rdd.collect().saveAsTextFile(folder_name)
-result_a.saveAsTextFile(folder_name + output_file_name_3a)
-print("a) Average number of stars per review for each city: {}".format(result_a.collect()))
+#result_a.saveAsTextFile(folder_name + output_file_name_3a)
+#print("a) Average number of stars per review for each city: {}".format(result_a.collect()))
 #print("b) Top 10 categories: {}".format(categories.takeOrdered(10, key=lambda x: -x[1])))
+print("c) {}".format(joined.collect()))
---
+++ task4.py
@@ -0,0 +1,54 @@
+from pyspark import SparkContext, SparkConf
+from datetime import datetime
+import numpy as np
+import csv
+import base64
+from math import sqrt
+
+def remove_header(csv):
+    csv_header = csv.first()
+    header = sc.parallelize([csv_header])
+    return csv.subtract(header)
+
+conf = SparkConf().setAppName("YelpReviews").setMaster("local")
+sc = SparkContext(conf=conf)
+
+folder_name = "./data/"
+
+input_file_name = "yelp_top_users_friendship_graph.csv"
+
+output_file_name = "result_1.csv"
+
+yelp_top_users_friendship_graph = remove_header(sc.textFile(folder_name + input_file_name)).map(lambda line: line.split(','))
+
+yelp_top_users_friendship_graph.cache()
+
+yelp_top_users_friendship_graph_rdd = yelp_top_users_friendship_graph.map(lambda fields: (fields[0], fields[1]))
+
+nodes_out_degrees = yelp_top_users_friendship_graph_rdd.map(lambda k: (k[0], 1)).reduceByKey(lambda x, y: x + y)
+
+nodes_in_degrees = yelp_top_users_friendship_graph_rdd.map(lambda k: (k[1], 1)).reduceByKey(lambda x, y: x + y)
+
+
+""" b """
+"""
+total_connections = nodes_in_degrees.values().sum()
+count_out = nodes_out_degrees.distinct().count()
+count_in = nodes_in_degrees.distinct().count()
+
+mean_out = total_connections / count_out
+mean_in = total_connections / count_in
+"""
+median_out = np.median(nodes_out_degrees.values().collect())
+median_in = np.median(nodes_in_degrees.values().collect())
+
+
+
+yelp_top_users_friendship_graph.unpersist()
+
+#print("a1) Top 10 nodes (out degrees): {}".format(nodes_out_degrees.takeOrdered(10, key=lambda x: -x[1])))
+#print("a2) Top 10 nodes (in degrees): {}".format(nodes_in_degrees.takeOrdered(10, key=lambda x: -x[1])))
+#print("b1) Mean of in degrees in friendships graph: {}".format(mean_in))
+#print("b2) Mean of out degrees in friendships graph: {}".format(mean_out))
+print("b3) Median of in degrees in friendships graph: {}".format(median_in))
+print("b4) Median of out degrees in friendships graph: {}".format(median_out))
